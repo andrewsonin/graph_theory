40b9a1d4bedc8ee4385ad13ade4a3927a36d64a6
Andrew Sonin <sonin.cel@gmail.com>
2017-03-20 16:27:34 +0300

    Update 05_tree_bfs.py

--- Graph_Theory_SemiFinal_Preparartion/05_tree_bfs.py
+++ Graph_Theory_SemiFinal_Preparartion/05_tree_bfs.py
@@ -1,34 +1,34 @@
-class task:
-
-    task= '''Остовное дерево поиска в ширину. Реализация на Python 3. O(N)'''
+# Остовное дерево поиска в ширину. Реализация на Python 3. O(N)
 
 def read_graph_as_lists():
-    N, M = tuple(map(int,input().split()))
-    graph = [[] for i in range(N)]
-    for edge in range(M):
+    vertexes, edges = tuple(map(int, input().split()))
+    graph = [[] for i in range(vertexes)]
+    for edge in range(edges):
         a, b = tuple(map(int, input().split()))
         graph[a].append(b)
         graph[b].append(a)
     return graph
-def bfs(graph, start, path, used = None):
-    if used == None:
+
+
+def bfs(graph, start, path, used=None):
+    if used is None:
         used = set()
     used.add(start)
-    Q = [start]
-    while Q:
-        vertex = Q.pop()
+    queue = [start]
+    while queue:
+        vertex = queue.pop()
         for neighbour in graph[vertex]:
             if neighbour not in used:
                 path.append((vertex, neighbour))
                 used.add(neighbour)
-                Q.append(neighbour)
+                queue.append(neighbour)
     return path
 
 
 def build_tree(graph):
     tree = []
-    used = set()
-    bfs(graph, 0, tree, used)
+    bfs(graph, 0, tree, set())
     return tree
+
 for edge in build_tree(read_graph_as_lists()):
-    print(*edge)
+    print(' '.join(map(str, edge)))
