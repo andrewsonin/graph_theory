b1deeb3c9f8509d16d545dfa8422ad698f4323c2
Andrew Sonin <sonin.cel@gmail.com>
2017-03-20 17:34:59 +0300

    Update 06_dijkstra_shortest_path_with_representation.py

--- Graph_Theory_SemiFinal_Preparartion/06_dijkstra_shortest_path_with_representation.py
+++ Graph_Theory_SemiFinal_Preparartion/06_dijkstra_shortest_path_with_representation.py
@@ -11,20 +11,20 @@ def read_graph_as_lists(vertex_number, edge_number):
 
 
 def dijkstra(graph, start, vertex_quantity):
-    used_vertexes = set()
+    used = set()
     path_length = [float('+inf')] * vertex_quantity
     path_length[start] = 0
     for i in range(vertex_quantity):
         min_distance = float('+inf')
         for vertex in range(vertex_quantity):
-            if vertex not in used_vertexes and path_length[vertex] < min_distance:
+            if vertex not in used and path_length[vertex] < min_distance:
                 current = vertex
                 min_distance = path_length[vertex]
         for neighbour in graph[current]:
             length = path_length[current] + neighbour[1]
             if length < path_length[neighbour[0]]:
                 path_length[neighbour[0]] = length
-        used_vertexes.add(current)
+        used.add(current)
     return path_length
 
 
