18bafe0a7819595f5208bccc864c093ff3ebfe69
Andrew Sonin <sonin.cel@gmail.com>
2017-03-20 18:00:52 +0300

    Update 08_Prim's_algorithm.py

--- Graph_Theory_SemiFinal_Preparartion/08_Prim's_algorithm.py
+++ Graph_Theory_SemiFinal_Preparartion/08_Prim's_algorithm.py
@@ -1,31 +1,22 @@
-class task:
+# Алгоритм построения минимального остовного дерева. Алгоритм Прима на неориентированном взвешенном связном графе
 
-    task= '''Алгоритм построения минимального остовного дерева. Алгоритм Прима на неориентированном взвешенном связном графе'''
 
-def read_as_lists():
-    N, M = tuple(map(int, input()))
-    graph = [[] for i in range(N)]
-    for i in range(M):
-        a, b, c = tuple(map(int, input().split()))
-        graph[a].append((b, c))
-        graph[b].append((a, c))
+def read_graph_as_lists(vertex_quantity, edge_quantity):
+    graph = [[] for i in range(vertex_quantity)]
+    for edge in range(edge_quantity):
+        link1, link2, weight = [int(x) for x in input().split()]
+        graph[link1].append((link2, weight)), graph[link2].append((link1, weight))
     return graph
-def prim(graph_as_lists, start_point):
-    costs = [float('inf') for i in range(len(graph_as_lists))] #матрица стоимостей добавления вершины к каркасу
-    costs[start_point] = 0
-    tree_nodes = [start_point]
-    tree_edges = []
-    tree_weight = 0
-    for i in range(len(graph_as_lists) - 1):
-        curr_min_cost = float('inf')
-        for vertex in tree_nodes:
-            for (neighbour, price) in graph_as_lists[vertex]:
-                if neighbour not in tree_nodes and price < curr_min_cost:
-                    curr_node = neighbour
-                    curr_min_cost = price
-                    curr_edge = (vertex, neighbour)
-        tree_nodes.append(curr_node)
-        tree_edges.append(curr_edge)
-        tree_weight += curr_min_cost
-        return tree_weight, tree_edges
 
+
+def prim(graph, vertex_quantity):
+    spanning_edges, spanning_vertexes, full_weight = [], {0}, 0
+    for i in range(vertex_quantity - 1):
+        min_length = float('+inf')
+        for vertex in spanning_vertexes:
+            for (neighbour, weight) in graph[vertex]:
+                if neighbour not in spanning_vertexes and weight < min_length:
+                    min_length, current_vertex, current_edges = weight, neighbour, (vertex, neighbour)
+        spanning_edges.append(current_edges), spanning_vertexes.add(current_vertex)
+        full_weight += min_length
+    return full_weight, spanning_edges
