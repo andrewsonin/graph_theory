9a65d5e2ca605311e41057a36176c197e1f253e1
Andrew Sonin <sonin.cel@gmail.com>
2017-03-20 19:52:07 +0300

    Update 10_topological_sort_tarjan.py

--- Graph_Theory_SemiFinal_Preparartion/10_topological_sort_tarjan.py
+++ Graph_Theory_SemiFinal_Preparartion/10_topological_sort_tarjan.py
@@ -1,42 +1,31 @@
-import sys
-class task:
-
-    task = '''Топологическая сортировка. Алгоритм Кана и Тарьяна. Реализация одного из них на Python 3.'''
+from sys import exit
 
 
-def read_graph_as_lists():
-    N, M = tuple(map(int, input().split()))
-    graph = [[] for i in range(N)]
-    for i in range(M):
-        a, b = tuple(map(int, input().split()))
-        graph[a].append(b)
+def read_graph_as_lists(vertex_quantity, edge_quantity):
+    graph = [[] for i in range(vertex_quantity)]
+    for edge in range(edge_quantity):
+        link1, link2 = [int(x) for x in input().split()]
+        graph[link1].append(link2)
     return graph
 
 
-def dfs_with_stack(graph, start, path, stack=None, used=None):
-    if stack == None:
-        stack = set()
-    if used == None:
-        used = set()
-    stack.add(start)
-    used.add(start)
-    for neighbour in graph[start]:
-        if neighbour in stack:
-            print('Not acyclic. Sorting Impossible.')
-            sys.exit(0)
-        if neighbour not in used:
-            dfs_with_stack(graph, neighbour, path, stack, used)
-    stack.remove(start)
-    path.append(start)
+def deep_first_search_tarjan(current_vertex, graph, sorted_path=None, used_vertexes=None, gray_vertexes=None):
+    gray_vertexes.add(current_vertex), used_vertexes.add(current_vertex)
+    for neighbour in graph[current_vertex]:
+        if neighbour not in used_vertexes:
+            deep_first_search_tarjan(neighbour, graph, sorted_path, used_vertexes, gray_vertexes)
+        elif neighbour in gray_vertexes:
+            print('NO')
+            exit(0)
+    gray_vertexes.remove(current_vertex), sorted_path.append(current_vertex)
 
 
-def tarjan_topological_sort(graph):
-    sorted_graph = []
-    used = set()
-    stack = set()
-    for node in range(len(graph)):
-        if node not in used:
-            dfs_with_stack(graph, node, sorted_graph, stack, used)
+def topological_sorting(graph, vertex_quantity):
+    sorted_vertexes, used_vertexes = [], set()
+    for vertex in range(vertex_quantity):
+        if vertex not in used_vertexes:
+            deep_first_search_tarjan(vertex, graph, sorted_vertexes, used_vertexes, set())
+    print(' '.join(map(str, sorted_vertexes[::-1])))
 
-    return sorted_graph[::-1]
-print(*tarjan_topological_sort(read_graph_as_lists()))
+vertex_number, edge_number = tuple(map(int, input().split()))
+topological_sorting(read_graph_as_lists(vertex_number, edge_number), vertex_number)
