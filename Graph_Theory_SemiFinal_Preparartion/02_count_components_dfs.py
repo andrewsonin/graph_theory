e0b550b6799d3174e390ac86b9aecd44adb4bf5d
Andrew Sonin <sonin.cel@gmail.com>
2017-03-20 15:20:46 +0300

    Update 02_count_components_dfs.py

--- Graph_Theory_SemiFinal_Preparartion/02_count_components_dfs.py
+++ Graph_Theory_SemiFinal_Preparartion/02_count_components_dfs.py
@@ -1,23 +1,25 @@
-class task:
-
-    task= '''Подсчёт компонент связности поиском в глубину. Реализация на Python 3. O(N*NUM_comp)'''
+# Подсчёт компонент связности поиском в глубину. Реализация на Python 3. O(N + NUM_comp) = O(N)
 
 
 def read_graph_as_lists():
-    N, M = tuple(map(int,input().split()))
-    graph = [[] for i in range(N)]
-    for edge in range(M):
+    vertexes, edges = tuple(map(int, input().split()))
+    graph = [[] for i in range(vertexes)]
+    for edge in range(edges):
         a, b = tuple(map(int, input().split()))
         graph[a].append(b)
         graph[b].append(a)
     return graph
-def dfs(graph, start, used = None):
-    if used == None:
+
+
+def dfs(graph, start, used=None):
+    if used is None:
         used = set()
     used.add(start)
     for vertex in graph[start]:
         if vertex not in used:
             dfs(graph, vertex, used)
+
+
 def count_components(graph):
     used = set()
     number_of_components = 0
@@ -28,4 +30,3 @@ def count_components(graph):
     return number_of_components
 
 print(count_components(read_graph_as_lists()))
-
